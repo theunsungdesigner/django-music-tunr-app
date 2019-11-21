7d5c36d923a8976401c00d681b89822093cf6ba1
theunsungdesigner <theunsungdesigner@gmail.com>
2019-11-21 12:19:15 -0500

    back end changes

--- tunr_project/tunr_app/models.py
+++ tunr_project/tunr_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    photo_url = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255, default='n/a')
+    album = models.CharField(max_length=255, default='n/a')
+    preview_url = models.TextField(default='n/a')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+
+#  this associates ID's for songs that reference each artist
--- tunr_project/tunr_project/settings.py
+++ tunr_project/tunr_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tunr_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'tunr_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tunr',
+        'USER': 'tunr_user',
+        'PASSWORD': 'tunr',
+        'HOST': 'localhost'
     }
 }
 
